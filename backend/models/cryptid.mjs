00546aa1fb4ada187033d1a4a65df8284e6a85b4
f.t <pero@peronoMacBook-Air.local>
2025-02-23 07:07:43 +0900

    Modify cryptidSchema

--- backend/models/cryptid.mjs
+++ backend/models/cryptid.mjs
@@ -2,47 +2,91 @@ import { Schema, model } from "mongoose";
 
 const cryptidSchema = new Schema(
   {
+    id: {
+      type: Number, // 画像フォルダの番号（事前に画像作成時に手動採番）
+      required: true,
+      unique: true,
+    },
     name: {
       type: String, 
       required: true,
       maxlength: 50,
-      default: '',
     },
+    alias: {
+      type: String,
+      default: '',
+      maxlength: 100,
+    }, 
     size: {
       type: String,
-      enum: ['S', 'M', 'L'],
-      maxlength: 1,
+      enum: ['S', 'M', 'L', 'XL'],
       required: true,
+    },
+    size_details: {
+      type: String,
+      default: '',
+      maxlength: 50,
     },
     area: {
       type: Number,
       required: true,
       min: 1,
     },
+    credibility: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
     description: {
       type: String,
       required: true,
       maxlength: 4000,
-      default: '',
+    },
+    main_sighting_location: {
+      type: String,
+      required: true,
+      maxlength: 100,
+    },
+    first_sighting: {
+      type: String,
+      required: true,
+      maxlength: 100,
     },
     video: {
       type: [String],
       default: [],
+      validate: {
+        validator: function (urls) {
+          return urls.every(url => /^https?:\/\/[\w.-]+(\.[\w.-]+)+[/#?]?.*$/.test(url));
+        },
+        message: "Invalid video URL format",
+      },
     },
     reference: {
       type: [
         {
-          title: { type: String },
-          link: { type: String },
+          title: { type: String, required: true },
+          link: { 
+            type: String, 
+            required: true,
+            validate: {
+              validator: function (url) {
+                return /^https?:\/\/[\w.-]+(\.[\w.-]+)+[/#?]?.*$/.test(url);
+              },
+              message: "Invalid reference URL format",
+            },
+          },
         },
       ],
       default: [],
     },
     related_uma: {
-      type: [Number],
+      type: [Number], // _idと紐づけるのではないのでSchema.Types.ObjectIdを使わない
       default: [],
     }
-  }
+  },
+  { timestamps: true } 
 );
 
 export const Cryptid = model("Cryptid", cryptidSchema);
