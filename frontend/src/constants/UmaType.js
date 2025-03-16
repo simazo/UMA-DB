04ea93505d80095e24ff052c899e3f7684f2247f
f.t <pero@peronoMacBook-Air.local>
2025-03-17 07:02:13 +0900

    UMA的分類を微調整

--- frontend/src/constants/UmaType.js
+++ frontend/src/constants/UmaType.js
@@ -4,7 +4,7 @@ export const UMA_TYPE = [
   { id: 3, icon: "image/i-orange-uma-3.svg", alt: "半獣半人 " },
   { id: 4, icon: "image/i-orange-uma-4.svg", alt: "恐竜・古代生物" },
   { id: 5, icon: "image/i-orange-uma-5.svg", alt: "精霊・悪魔" },
-  { id: 6, icon: "image/i-orange-uma-6.svg", alt: "絶滅・貴重種" },
-  { id: 7, icon: "image/i-orange-uma-7.svg", alt: "怪物" },
+  { id: 6, icon: "image/i-orange-uma-6.svg", alt: "絶滅危惧種" },
+  { id: 7, icon: "image/i-orange-uma-7.svg", alt: "訪問者" },
   { id: 8, icon: "image/i-orange-uma-8.svg", alt: "その他" },
 ];
