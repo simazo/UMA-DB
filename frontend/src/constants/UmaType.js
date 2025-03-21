a69428dbcdce93e0ab93bc5648e75a45a8ed4726
f.t <pero@peronoMacBook-Air.local>
2025-03-21 12:47:25 +0900

    文言変更

--- frontend/src/constants/UmaType.js
+++ frontend/src/constants/UmaType.js
@@ -1,10 +1,10 @@
 export const UMA_TYPE = [
-  { id: 1, icon: "image/i-orange-uma-1.svg", alt: "水棲首長竜" },
+  { id: 1, icon: "image/i-orange-uma-1.svg", alt: "水龍・海蛇" },
   { id: 2, icon: "image/i-orange-big-foot.svg", alt: "類人猿" },
   { id: 3, icon: "image/i-orange-uma-3.svg", alt: "半獣半人 " },
   { id: 4, icon: "image/i-orange-uma-4.svg", alt: "恐竜・古代生物" },
   { id: 5, icon: "image/i-orange-uma-5.svg", alt: "精霊・悪魔" },
-  { id: 6, icon: "image/i-orange-uma-6.svg", alt: "絶滅危惧種" },
+  { id: 6, icon: "image/i-orange-uma-6.svg", alt: "絶滅種" },
   { id: 7, icon: "image/i-orange-uma-7.svg", alt: "訪問者" },
   { id: 8, icon: "image/i-orange-uma-8.svg", alt: "その他" },
 ];
