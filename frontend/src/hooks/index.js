0a1b1c3dca51a2872f0defac927c86f3c08a1b86
f.t <pero@peronoMacBook-Air.local>
2025-04-14 13:45:49 +0900

    クエリパラメータ処理をカスタムフックに分離

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -2,3 +2,4 @@ export { default as useCryptidCount } from "./useCryptidCount";
 export { default as useLatestCryptids } from "./useLatestCryptids";
 export { default as useCryptids } from "./useCryptids";
 export { default as useHandleSearch } from "./useHandleSearch";
+export { default as useCryptidQueryParams } from "./useCryptidQueryParams";
