9d8de5014b37d801ac8f00899d43b48dec885d73
f.t <pero@peronoMacBook-Air.local>
2025-04-14 12:06:52 +0900

    HomeにContainer/Presentationalパターン適用

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,3 @@
-export { default as Home } from "./Home/Home";
+export { default as HomePage } from "./Home/HomePage";
 export { default as CryptidList } from "./CryptidList";
 export { default as Cryptid } from "./Cryptid";
