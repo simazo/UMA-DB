a3907a1ebd17af91a9e8711a901f8dce35d11e86
f.t <pero@peronoMacBook-Air.local>
2025-06-29 17:44:02 +0900

    id設定ミス修正

--- frontend/src/pages/Cryptid/CryptidView.jsx
+++ frontend/src/pages/Cryptid/CryptidView.jsx
@@ -11,7 +11,7 @@ import {
 
 const CryptidView = ({cryptid}) => {
   const tweetText = `${cryptid.name}に関する情報はこちら`;
-  const ogpUrl = `https://uma-db.com/ogp/cryptids/${cryptid._id}.html`;
+  const ogpUrl = `https://uma-db.com/ogp/cryptids/${cryptid.id}.html`;
   const hashtags = ["UMA", "未確認生物", cryptid.name, ...(cryptid.alias ? [cryptid.alias] : [])];
 
   return (
